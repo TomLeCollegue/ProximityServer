f644ad63e8169b06c65af1429e01d88484bf3dfc
Kubasik <tomkubasik.pro@gmail.com>
2020-11-01 15:33:05 +0100

    Compression for little images

--- RestFullTEST/src/main/java/rest/ImagesRest.java
+++ RestFullTEST/src/main/java/rest/ImagesRest.java
@@ -1,14 +1,23 @@
 package rest;
 
+
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.rmi.RemoteException;
+import java.util.Iterator;
 
 @Path("/images")
 public class ImagesRest {
@@ -28,6 +37,49 @@ public class ImagesRest {
         byte[] _array = Files.readAllBytes(path);
         return _array;
     }
+
+    @GET
+    @Path("/{email}/downloadLow")
+    @Produces("text/plain")
+    public byte[] downloadLow(@PathParam("email") String email) throws IOException {
+        String fileName = email + "_pic.jpg";
+        return get(fileName);
+
+    }
+
+    //compressImagesBeforeSending
+    public static byte[] getLow(String _fileName) throws RemoteException, IOException, OutOfMemoryError{
+        java.nio.file.Path path = Paths.get("C:\\Users\\Tom\\Desktop\\Proximity\\ProximityServer\\RestFullTEST\\images\\" + _fileName);
+
+        File input = new File(String.valueOf(path));
+        BufferedImage image;
+        image = ImageIO.read(input);
+
+        File compressedImageFile = new File("C:\\Users\\Tom\\Desktop\\Proximity\\ProximityServer\\RestFullTEST\\images\\compressed_" + _fileName);
+        OutputStream os = new FileOutputStream(compressedImageFile);
+
+        Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("jpg");
+        ImageWriter writer = (ImageWriter) writers.next();
+
+        ImageOutputStream ios = ImageIO.createImageOutputStream(os);
+        writer.setOutput(ios);
+
+        ImageWriteParam param = writer.getDefaultWriteParam();
+
+        param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+        param.setCompressionQuality(0.005f);  // Change the quality value you prefer
+        writer.write(null, new IIOImage(image, null, null), param);
+
+        os.close();
+        ios.close();
+        writer.dispose();
+
+        java.nio.file.Path pathCompressed = Paths.get("C:\\Users\\Tom\\Desktop\\Proximity\\ProximityServer\\RestFullTEST\\images\\compressed_" + _fileName);
+        byte[] _array = Files.readAllBytes(pathCompressed);
+
+        return _array;
+    }
+
 
 
     @POST
